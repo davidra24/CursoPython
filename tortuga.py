c489382a7540f358b7cea01cdbd6072795fc86d1
David Ramirez <cdavidramirezg24@gmail.com>
2019-05-09 11:47:24 -0500

    turtle example

---
+++ tortuga.py
@@ -0,0 +1,25 @@
+import turtle
+
+window = turtle.Screen()
+
+tortuga = turtle.Turtle()
+tortuga.forward(100)
+tortuga.right(90)
+tortuga.forward(100)
+tortuga.right(90)
+tortuga.forward(100)
+tortuga.right(90)
+tortuga.forward(100)
+tortuga.right(90)
+
+colors=['red','purple','blue','green','yellow','orange']
+
+t = turtle.Pen()
+t.speed(0)
+for x in range(360):
+    t.pencolor(colors[x%6])
+    t.width(x/100+1)
+    t.forward(x)
+    t.left(59)
+
+window.mainloop()
